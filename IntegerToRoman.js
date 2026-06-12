3cb93157c722e83e20f84ce0d5f401ebec62d849
arko1995 <shahriarsohelarko@gmail.com>
2026-06-12 14:48:18 +0600

    top interview questions

---
+++ IntegerToRoman.js
@@ -0,0 +1,32 @@
+const intToRoman = function (num) {
+  let result = "";
+
+  const numerals = [
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ];
+
+  for (let ch of numerals) {
+    const [int, roman] = ch;
+
+    while (num >= int) {
+      num = num - int;
+      result += roman;
+    }
+  }
+
+  return result;
+};
+
+intToRoman(3749);
