b573f8bd0f49401b7c868c9af209ea72f7642e5d
arko1995 <shahriarsohelarko@gmail.com>
2026-06-22 15:53:16 +0600

    sql solution

--- EmployeeBonus.sql
+++ EmployeeBonus.sql
@@ -1,5 +1,5 @@
 SELECT Employee.name, Bonus.bonus
 FROM Employee
 LEFT JOIN Bonus
-ON Employee.empid = Bonus.empId
-WHERE Bonus.bonus <= 1000 OR Bonus.bonus IS NULL
+ON Employee.empId = Bonus.empId
+WHERE Bonus.bonus < 1000 OR Bonus.bonus IS NULL
