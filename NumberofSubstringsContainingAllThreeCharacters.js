890009952d354684ecb2f312b43cd6e89f51465a
arko1995 <shahriarsohelarko@gmail.com>
2026-06-30 17:10:36 +0600

    daily challenge

---
+++ NumberofSubstringsContainingAllThreeCharacters.js
@@ -0,0 +1,26 @@
+const numberOfSubstrings = function (s) {
+  let output = 0;
+  let left = 0;
+
+  let count = {
+    a: 0,
+    b: 0,
+    c: 0,
+  };
+
+  for (let right = 0; right < s.length; right++) {
+    count[s[right]]++;
+
+    while (count.a > 0 && count.b > 0 && count.c > 0) {
+      output += s.length - right;
+      count[s[left]]--;
+      left++;
+    }
+  }
+
+  console.log(output);
+
+  return output;
+};
+
+numberOfSubstrings("abcabc");
