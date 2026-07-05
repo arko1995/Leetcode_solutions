ff1ad4ad6b4912804b74b367f7551da9608366ef
arko1995 <shahriarsohelarko@gmail.com>
2026-07-06 03:14:58 +0600

    random

---
+++ IntersectionofTwoLinkedLists.js
@@ -0,0 +1,17 @@
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+const getIntersectionNode = function (headA, headB) {
+  if (!headA || !headB) return null;
+
+  let a = headA;
+  let b = headB;
+
+  while (a !== b) {
+    a = a === null ? headB : a.next;
+    b = b === null ? headA : b.next;
+  }
+  return a;
+};
